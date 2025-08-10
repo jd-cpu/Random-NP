189831fd54fa1b4a282f3dad471cc3465db62bd0
jd-cpu <bompser@naver.com>
2025-08-10 21:28:06 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
 self.addEventListener('install', event => {
-  // 매 설치 시마다 캐시 비우기
   event.waitUntil(
     caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => caches.delete(cache))
-      );
+      return Promise.all(cacheNames.map(cache => caches.delete(cache)));
     })
   );
   self.skipWaiting();
@@ -15,7 +12,6 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  // 항상 네트워크에서 먼저 가져오고, 실패 시 캐시 사용
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
